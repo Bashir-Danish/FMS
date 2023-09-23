37c076e00ee2456b8549078f1e6d7dac8b49a230
Bashir-Danish <bashirdanish124@gmail.com>
2023-09-23 19:33:20 +0430

    auth tested

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,15 +1,16 @@
 import jwt from "jsonwebtoken";
 
-
 export const isAuthenticatedUser = async (req, res, next) => {
   const token =
     req.headers["x-access-token"] ||
     req.headers["authorization"] ||
     req.headers["x-token"] ||
     req.query.token;
-  
+  console.log(token);
   if (!token) {
-    return res.status(401).json({ message: "Please Login to access this resource" });
+    return res
+      .status(401)
+      .json({ message: "Please Login to access this resource" });
   }
 
   jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
@@ -18,6 +19,8 @@ export const isAuthenticatedUser = async (req, res, next) => {
       return res.status(401).json({ error: "Invalid token" });
     }
     req.decodedData = decoded;
+    console.log(decoded);
+
     next();
   });
 };
